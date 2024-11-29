d2f0553050fbbffb1cd26fbade81264562076a07
Ishan Kapur <kapuri2@mcmaster.ca>
2024-11-29 01:11:13 -0500

    message

--- Player.cpp
+++ Player.cpp
@@ -34,53 +34,35 @@ void Player::updatePlayerDir()
     {
         switch(input)
         {                      
-            case 8:  // exit with backspace
-                exitFlag = 1;
-                break;
 
             case 'w':
             case 'W':
-                if (direction == RIGHT||direction == LEFT|| direction == STOP){
+                if (myDir == RIGHT||myDir == LEFT|| myDir == STOP){
 
-                    direction = UP;
+                    myDir= UP;
                 }
                 break;
             case 'a':
             case 'A':
-                if (direction == UP||direction == DOWN|| direction == STOP){
+                if (myDir == UP||myDir == DOWN|| myDir == STOP){
 
-                    direction = LEFT;
+                    myDir = LEFT;
                 }
                 break;
             case 's':
             case 'S':
-                if (direction == RIGHT||direction == LEFT|| direction == STOP){
+                if (myDir == RIGHT||myDir == LEFT|| myDir == STOP){
 
-                    direction = DOWN;
+                    myDir = DOWN;
                 }
                 break;
             case 'd':
             case 'D':
-                if (direction == UP||direction == DOWN|| direction == STOP){
+                if (myDir == UP||myDir == DOWN|| myDir == STOP){
 
-                    direction = RIGHT;
+                    myDir = RIGHT;
                 }
                 break;   
-            case '1':
-                speed = 225000;
-                break;
-            case '2':
-                speed = 175000;
-                break;
-            case '3':
-                speed = 125000;
-                break;
-            case '4':
-                speed = 100000;
-                break;
-            case '5':
-                speed = 75000;
-                break;
 
             default:
                 break;
@@ -93,28 +75,28 @@ void Player::updatePlayerDir()
 void Player::movePlayer()
 {
     // PPA3 Finite State Machine logic
- if (direction == UP){
-        object.y = (object.y-1);
-        if (object.y == 0){
-            object.y = 8;
+ if (myDir == UP){
+        playerPos.pos->y = (playerPos.pos->y-1);
+        if (playerPos.pos->y == 0){
+            playerPos.pos->y = 8;
         }
     }
-    if (direction == DOWN){
-        object.y = (object.y+1);
-        if (object.y == 9){
-            object.y = 1;
+    if (myDir == DOWN){
+        playerPos.pos->y = (playerPos.pos->y+1);
+        if (playerPos.pos->y == 9){
+            playerPos.pos->y = 1;
         }
     }
-    if (direction == RIGHT){
-        object.x = (object.x+1);
-        if (object.x == 19){
-            object.x = 1;
+    if (myDir == RIGHT){
+        playerPos.pos->x = (playerPos.pos->x+1);
+        if (playerPos.pos->x == 19){
+            playerPos.pos->x = 1;
         }
     }
-    if (direction == LEFT){
-        object.x = (object.x-1);
-        if (object.x == 0){
-            object.x = 18;
+    if (myDir == LEFT){
+        playerPos.pos->x = (playerPos.pos->x-1);
+        if (playerPos.pos->x == 0){
+            playerPos.pos->x = 18;
         }
     }
 }
